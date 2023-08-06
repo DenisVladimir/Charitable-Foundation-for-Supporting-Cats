e18ccc65aca0580e1f5fd4f65759481054fddd06
DenisVladimir <statuspobeda@yandex.com>
2023-08-06 17:25:11 +0300

    Commit7

--- app/services/investing_services.py
+++ app/services/investing_services.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from typing import List, Union
+
+from app.crud.base import CRUDBase
 from app.crud.charity_project import CRUDCharityProject
 from app.crud.donation import CRUDDonation
 
@@ -7,13 +9,14 @@ from app.crud.donation import CRUDDonation
 def investment(
     target: Union[CRUDCharityProject, CRUDDonation],
     sourses: List[Union[CRUDCharityProject, CRUDDonation]]
-) -> List[Union[CRUDCharityProject, CRUDDonation]]:
+) -> List[CRUDBase]:
     """
     Перебирает открытые проекты/пожертвования,
     закрывает пожертвования/проекты при достижении лимита.
     При создании пожертвования перебирает открытые проекты.
     При создании проекта перебирает свободные пожертвования.
     """
+    changed_objects = []
     for open_obj in sourses:
         """Перераспределяет средства между проектами и пожертвованиями."""
         append_obj = min(
@@ -27,4 +30,6 @@ def investment(
             if object.invested_amount == open_obj.full_amount:
                 object.fully_invested = True
                 object.close_date = datetime.now()
-    return sourses
+            if object in sourses:
+                changed_objects.append(object)
+    return changed_objects
