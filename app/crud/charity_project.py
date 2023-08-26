5684a55e734f68517d27a1f5b25b8e7d75d91de5
DenisVladimir <statuspobeda@yandex.ru>
2023-08-26 15:04:56 +0300

    Commit1

--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -1,6 +1,9 @@
+from typing import Optional
+
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, func
 from app.models import CharityProject
+
 
 from app.crud.base import CRUDBase
 
@@ -18,5 +21,21 @@ class CRUDCharityProject(CRUDBase):
         )
         return db_project_id.scalar()
 
+        # Функция сортировки проектов по времени закрытия
+    async def get_projects_by_completion_rate(
+            self,
+            session: AsyncSession,
+    ) -> list[dict]:
+        close_projects = await session.execute(
+            select(
+                CharityProject.name,
+                func.julianday(CharityProject.close_date) -
+                func.julianday(CharityProject.create_date),
+                CharityProject.description).where(
+                    CharityProject.fully_invested == 1
+            ).order_by(CharityProject.close_date - CharityProject.create_date)
+        )
+        close_projects = close_projects.all()
+        return close_projects
 
 charity_project_crud = CRUDCharityProject(CharityProject)
